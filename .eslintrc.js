ff24c07918f920bed74d2c585e68f5ea4c0f5741
Benjamin Pak <benjaminjpak95@gmail.com>
2023-07-11 20:04:33 -0700

    Edited package.json, eslint, webpack.config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,25 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        globals: true,
-        node: true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: [
-        "eslint:recommended",
-        "airbnb",
-        "prettier",
-        "plugin:react/recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:import/errors",
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: ["react", "prettier", "import", "jsx-ally"],
-    rules: {
-        "prettier/prettier": ["error"],
-        "no-console": "off",
-        "no-restricted-syntax": ["error"],
-        "import/extensions": ["error", "never", { "jsx": "always" }],
-        "react/prop-types": "off",
-        "no-multi-str": "off"
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-}
+  },
+};
---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Landing() {
+  return (
+    <div id="Landing">
+      <h1>Portfolio</h1>
+    </div>
+  );
+}
