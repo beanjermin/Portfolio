84c870a3d8cae253c5c56e77086a6f226d0917f2
Benjamin Pak <benjaminjpak95@gmail.com>
2023-07-20 18:51:25 -0700

    Finished skeleton of about page, implemented syntax highligther, finished icon carousel

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {
   StyledAboutHeader,
   StyledAboutContentContainer,
@@ -7,8 +9,10 @@ import {
   StyledPic,
   StyledBio,
   StyledLeetCode,
+  StyledCarouselContainer,
   StyledCarousel,
 } from './about.styled';
+import { TechIcons } from './TechIcons';
 
 export default function About() {
   const { ref: myRef, inView: visible } = useInView();
@@ -30,24 +34,149 @@ export default function About() {
   */
 
   return (
-    <section id="About">
+    <section id="About" style={{ position: 'relative' }}>
       <StyledAboutHeader ref={myRef}>
         About
       </StyledAboutHeader>
       <StyledAboutContentContainer>
         <StyledAboutContent>
           <StyledPic>
-            <div>picture here</div>
+            <img
+              style={{ width: 400, height: 300 }}
+              src="file:///Users/benpak/Pictures/StudioGWallPapers/PM5.jpeg"
+              alt="placeholder"
+            />
           </StyledPic>
           <StyledBio>
-            HELLO MY NAME IS BENJAMIN. JUST GRADUATED HACK REACTOR. PLEASE HIRE ME!
+            <div>
+              HELLO MY NAME IS BENJAMIN. JUST GRADUATED HACK REACTOR. PLEASE HIRE ME!
+              BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH
+              BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH
+              BLAH BLAHBLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH
+              BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH
+              BLAH BLAHBLAH BLAH BLAH BLAH BLAH
+              BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAH
+              BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH
+              BLAHBLAH BLAH BLAH BLAH BLAH
+              BLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH BLAH BLAHBLAH BLAH BLAH
+              BLAH BLAHBLAH BLAH BLAH BLAH BLAH
+            </div>
           </StyledBio>
           <StyledLeetCode>
-            CODE CODE CODE
+            Daily Leetcode:
+            <strong>
+              Best Time to Buy and Sell Stock
+            </strong>
+            <SyntaxHighlighter className="code-block" language="javascript" style={vs2015} showLineNumbers>
+              {
+`/*
+Problem Analysis:
+  Two things we're trying to determine:
+  when to buy and when to sell
+  We want to maximize profit, but profits
+  can only be made in buy/sell order
+  We want to buy at the lowest possible price,
+  and sell at the highest possible price
+Planning:
+  The idea is to iterate over the array
+  while keeping track of the max profits.
+  Create a buy variable. This is a placeholder
+  variable that will represent the lowest possible price.
+  Create a profit variable to keep track of max profits
+*/
+
+var maxProfit = function(prices) {
+    // set buy variable to the first number by default
+    let buy = prices[0];
+    // create profit var
+    let profit = 0;
+
+    // iterate over the input arr
+    // (note: i starts at 1 because we accounted for the first number in the buy var)
+    for (let i = 1; i < prices.length; i++) {
+        // we're looking for the lowest possible price to buy.
+        // if buy value is greater than the current price,
+        // we want to replace the buy var with the lower number
+        if (buy > prices[i]) {
+            buy = prices[i];
+        // if however, the current price is greater than the current buy variable
+        // (which satisfies the buy/sell order for profit [sell day comes after buy day]),
+        // we want to keep track of the profit
+        } else {
+            // Math.max will take multiple parameters and output the biggest number.
+            // Here we are comparing the new potential profit with the existing profit.
+            // This way, we can save the max profit amount
+            profit = Math.max(prices[i] - buy, profit);
+        }
+    }
+    // then, simply return the max profit amount
+    return profit;
+};`
+              }
+            </SyntaxHighlighter>
           </StyledLeetCode>
         </StyledAboutContent>
-        <StyledCarousel> tech stack carousel here</StyledCarousel>
       </StyledAboutContentContainer>
+      <StyledCarouselContainer>
+        <StyledCarousel>
+          <img src={TechIcons.javascript} alt="javascript icon" />
+          <img src={TechIcons.react} alt="react icon" />
+          <img src={TechIcons.express} alt="express icon" />
+          <img src={TechIcons.node} alt="node icon" />
+          <img src={TechIcons.mongodb} alt="mongodb icon" />
+          <img src={TechIcons.postgres} alt="postgres icon" />
+          <img src={TechIcons.mysql} alt="mysql icon" />
+          <img src={TechIcons.html} alt="html icon" />
+          <img src={TechIcons.css} alt="css icon" />
+          <img src={TechIcons.git} alt="git icon" />
+          <img src={TechIcons.github} alt="github icon" />
+          <img src={TechIcons.rest} alt="rest icon" />
+          <img src={TechIcons.websocket} alt="websocket icon" />
+          <img src={TechIcons.vscode} alt="vscode icon" />
+          <img src={TechIcons.postman} alt="postman icon" />
+          <img src={TechIcons.sass} alt="sass icon" />
+          <img src={TechIcons.tailwind} alt="tailwind icon" />
+          <img src={TechIcons.figma} alt="figma icon" />
+          <img src={TechIcons.npm} alt="npm icon" />
+          <img src={TechIcons.jest} alt="jest icon" />
+          <img src={TechIcons.chai} alt="chai icon" />
+          <img src={TechIcons.mocha} alt="mocha icon" />
+          <img src={TechIcons.webpack} alt="webpack icon" />
+          <img src={TechIcons.bash} alt="bash icon" />
+          <img src={TechIcons.docker} alt="docker icon" />
+          <img src={TechIcons.nginx} alt="nginx icon" />
+          <img src={TechIcons.aws} alt="aws icon" />
+        </StyledCarousel>
+        <StyledCarousel>
+          <img src={TechIcons.javascript} alt="javascript icon" />
+          <img src={TechIcons.react} alt="react icon" />
+          <img src={TechIcons.express} alt="express icon" />
+          <img src={TechIcons.node} alt="node icon" />
+          <img src={TechIcons.mongodb} alt="mongodb icon" />
+          <img src={TechIcons.postgres} alt="postgres icon" />
+          <img src={TechIcons.mysql} alt="mysql icon" />
+          <img src={TechIcons.html} alt="html icon" />
+          <img src={TechIcons.css} alt="css icon" />
+          <img src={TechIcons.git} alt="git icon" />
+          <img src={TechIcons.github} alt="github icon" />
+          <img src={TechIcons.rest} alt="rest icon" />
+          <img src={TechIcons.websocket} alt="websocket icon" />
+          <img src={TechIcons.vscode} alt="vscode icon" />
+          <img src={TechIcons.postman} alt="postman icon" />
+          <img src={TechIcons.sass} alt="sass icon" />
+          <img src={TechIcons.tailwind} alt="tailwind icon" />
+          <img src={TechIcons.figma} alt="figma icon" />
+          <img src={TechIcons.npm} alt="npm icon" />
+          <img src={TechIcons.jest} alt="jest icon" />
+          <img src={TechIcons.chai} alt="chai icon" />
+          <img src={TechIcons.mocha} alt="mocha icon" />
+          <img src={TechIcons.webpack} alt="webpack icon" />
+          <img src={TechIcons.bash} alt="bash icon" />
+          <img src={TechIcons.docker} alt="docker icon" />
+          <img src={TechIcons.nginx} alt="nginx icon" />
+          <img src={TechIcons.aws} alt="aws icon" />
+        </StyledCarousel>
+      </StyledCarouselContainer>
     </section>
   );
 }
--- client/src/components/About/about.styled.js
+++ client/src/components/About/about.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const StyledAboutHeader = styled.div`
   margin: auto;
@@ -14,26 +14,89 @@ export const StyledAboutContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
-  top: 12%;
+  top: 8%;
 `;
 
 export const StyledAboutContent = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
   height: 50vh;
 `;
 
 export const StyledPic = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left: 20px;
+  margin-right: 40px;
 `;
 
 export const StyledBio = styled.div`
+  margin-right: 40px;
+  overflow: scroll;
 `;
 
 export const StyledLeetCode = styled.div`
+  grid-column: span 2;
+  min-width: 500px;
+  width: 100%;
+  height: 300px;
+  max-height: 300px;
+  .code-block {
+    height: 400px;
+    width: 95%;
+    border: 2px solid black;
+    border-radius: 10px;
+  }
+`;
+
+const slide = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
 `;
 
 export const StyledCarousel = styled.div`
-  height: 22vh;
-  background: red;
+  display: inline-block;
+  padding-top: 25px;
+  animation: 65s ${slide} infinite linear;
+  > img {
+    height: 100px;
+    width: 100px;
+    margin: 0 35px;
+  }
+  `;
+
+export const StyledCarouselContainer = styled.div`
+  height: 17.5vh;
+  background: lightgrey;
+  overflow: hidden;
+  white-space: nowrap;
+  position: absolute;
+  bottom: 0;
+  &:hover ${StyledCarousel} {
+    animation-play-state: paused;
+  }
+  &:before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 150px;
+    height: 100%;
+    background: linear-gradient(to left, rgba(255, 255, 255, 0), white);
+    content: '';
+    z-index: 2;
+  }
+  &:after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 150px;
+    height: 100%;
+    background: linear-gradient(to right, rgba(255, 255, 255, 0), white);
+    content: '';
+    z-index: 2;
+  }
 `;
